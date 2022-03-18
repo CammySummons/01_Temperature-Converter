71d79bf21223cc88a673c2cb4cfe40c902e7258c
CammySummons <64822645+CammySummons@users.noreply.github.com>
2022-03-18 16:57:50 +1300

    Update 01_Help_GUI_v5.py

--- 01_Help_GUI_v5.py
+++ 01_Help_GUI_v5.py
@@ -1,8 +1,6 @@
 from tkinter import *
 from functools import partial  # To prevent unwanted windows
 
-import random
-
 
 class Converter:
     def __init__(self):
@@ -11,7 +9,8 @@ class Converter:
         background_color = "light blue"
 
         # Converter Main Screen GUI...
-        self.converter_frame = Frame(width=300, height=300, bg=background_color, pady=10)
+        self.converter_frame = Frame(width=300, height=300,
+                                     bg=background_color, pady=10)
         self.converter_frame.grid()
 
         # Temperature Conversion Heading (row 0)
@@ -23,8 +22,8 @@ class Converter:
         self.temp_converter_label.grid(row=0)
 
         # Help Button (row 1)
-        self.help_button = Button(self.converter_frame, text="Help", font=("Arial", "14"),
-                                  padx=10, pady=10, command=self.help)
+        self.help_button = Button(self.converter_frame, text="Help", font=(
+            "Arial", "14"), padx=10, pady=10, command=self.help)
         self.help_button.grid(row=1)
 
     def help(self):
@@ -44,7 +43,8 @@ class Help:
         self.help_box = Toplevel()
 
         # If users press cross at top, closes help and 'releases' help button
-        self.help_box.protocol('WM_DELETE_WINDOW', partial(self.close_help, partner))
+        self.help_box.protocol('WM_DELETE_WINDOW',
+                               partial(self.close_help, partner))
 
         # Set up GUI Frame
         self.help_frame = Frame(self.help_box, width=300, bg=background)
@@ -57,13 +57,13 @@ class Help:
 
         # Help text (label, row 1)
         self.help_text = Label(self.help_frame, text="",
-                                justify=LEFT, width=40, bg=background, wrap=250)
+                               justify=LEFT, width=40, bg=background, wrap=250)
         self.help_text.grid(row=1)
 
         # Dismiss button (row 2)
         self.dismiss_btn = Button(self.help_frame, text="Dismiss",
-                                    width=10, bg="orange", font="arial 10 bold",
-                                    command=partial(self.close_help, partner))
+                                  width=10, bg="orange", font="arial 10 bold",
+                                  command=partial(self.close_help, partner))
         self.dismiss_btn.grid(row=2, pady=10)
 
     def close_help(self, partner):
